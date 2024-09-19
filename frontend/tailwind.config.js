e51ef749961ff96f7e8410be2c65bf9a9462a696
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-09-19 18:06:52 +0530

    home component Done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts,scss}"],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       lineHeight: {
         60: "3.75rem",
